f62c8992b68736dac3726de9f69305d097e3313c
dubdub01 <80472211+dubdub01@users.noreply.github.com>
2023-09-28 12:46:12 +0200

    setup home pc

--- migrations/Version20230928102624.php
+++ migrations/Version20230928102624.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230918120949 extends AbstractMigration
+final class Version20230928102624 extends AbstractMigration
 {
     public function getDescription(): string
     {
